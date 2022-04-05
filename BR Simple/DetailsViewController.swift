86519b89393890dca39506fd439905756e927eb7
vucjanac <lekavucje@gmail.com>
2022-04-05 19:35:18 +0200

    added additional info to DetailsViewControlloer
    failed to load data, missing Coding Keys(?)

--- BR Simple/DetailsViewController.swift
+++ BR Simple/DetailsViewController.swift
@@ -6,12 +6,56 @@
 //
 
 import UIKit
+import MapKit
 
-final class DetailsViewController: UIViewController {
+final class DetailsViewController: UIViewController, MKMapViewDelegate {
+    @IBOutlet var image: UIImageView!
+    @IBOutlet var name: UILabel!
+    @IBOutlet var category: UILabel!
+    @IBOutlet var address: UILabel!
+    @IBOutlet var phone: UILabel!
+    @IBOutlet var social: UILabel!
+    @IBOutlet var mapKitView: MKMapView!
+    
+    var restaurant: RestaurantsModel?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
+        updateMap()
         
-        view.backgroundColor = .white
+        view.backgroundColor = .lightGray
     }
+    
+    func updateUI() {
+        
+        self.name.text = restaurant?.restaurantsName
+        self.category.text = restaurant?.restaurantsCategory
+        self.address.text = restaurant?.location.address
+        self.phone.text = restaurant?.contact?.formattedPhone
+        self.social.text = restaurant?.contact?.twitter
+    }
+    
+    func updateMap() {
+        
+        let lattitude: CLLocationDegrees = (restaurant?.location.lat)!
+        let longitude: CLLocationDegrees = (restaurant?.location.lng)!
+        
+        let latDelta: CLLocationDegrees = 0.01
+        let lonDelta: CLLocationDegrees = 0.01
+        
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        let center: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: lattitude, longitude: longitude)
+        
+        let location: MKCoordinateRegion = MKCoordinateRegion(center: center, span: span)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = center
+        annotation.title = restaurant?.restaurantsName
+        
+        mapKitView.addAnnotation(annotation)
+        mapKitView.setRegion(location, animated: true)
+    }
+        
 }
--- BR Simple/Restaurants.swift
+++ BR Simple/Restaurants.swift
@@ -16,12 +16,9 @@ struct RestaurantsModel: Codable {
     let restaurantsName: String
     let restaurantsCategory: String
     let backgroundImageURL: String
+    let contact: RestaurantContact?
+    let location: RestaurantLocation
     
-    enum CodingKeys: String, CodingKey {
-        case restaurantsName = "name"
-        case restaurantsCategory = "category"
-        case backgroundImageURL
-    }
     var bgImage: UIImage {
         if let url = URL(string: backgroundImageURL),
            let data = try? Data(contentsOf: url) {
@@ -31,4 +28,36 @@ struct RestaurantsModel: Codable {
         }
     }
     
+}
+
+struct RestaurantLocation: Codable {
+    let address: String
+    let crossStreet: String?
+    let lat: Double
+    let lng: Double
+    let postalCode: String?
+    let cc: String
+    let city: String
+    let state: String
+    let country: String
+    let formattedAddress: [String]
+}
+
+struct RestaurantContact: Codable {
+    let phone: String
+    let formattedPhone: String
+    let twitter: String?
+    let facebook: String?
+    let facebookUsername: String?
+    let facebookName : String?
+    
+}
+
+
+enum CodingKeys: String, CodingKey {
+    case restaurantsName = "name"
+    case restaurantsCategory = "category"
+    case backgroundImageURL
+    case RestaurantLocation
+    case RestaurantContact
 }
