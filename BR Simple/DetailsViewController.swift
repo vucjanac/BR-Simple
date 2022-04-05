49a0484f77ae595ee58e69ae6a7fbb3a7b9d1399
vucjanac <lekavucje@gmail.com>
2022-04-05 13:14:22 +0200

    changed background color to white

--- BR Simple/DetailsViewController.swift
+++ BR Simple/DetailsViewController.swift
@@ -12,6 +12,6 @@ final class DetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .lightGray
+        view.backgroundColor = .white
     }
 }
